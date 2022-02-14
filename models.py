bc02448a7019ca7243b4454be762f55252d130a8
PaulErpenstein <paul.erpenstein@gmail.com>
2022-02-14 09:51:12 +0100

    Safety commit, loads of models and results

--- models.py
+++ models.py
@@ -2,13 +2,15 @@ import sklearn.metrics as metrics
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import LinearSVC
 from sklearn.neural_network import MLPClassifier
 
-def do_classification_report(y_true, y_pred, name):
+def do_classification_report(y_true, y_pred, name, file_path=""):
     print("Classification report for {}".format(name))
     print("Accuracy score: {}".format(metrics.accuracy_score(y_true, y_pred)))
     print("Recall score: {}".format(metrics.recall_score(y_true, y_pred, average="weighted")))
@@ -28,34 +30,44 @@ def do_classification_report(y_true, y_pred, name):
     plt.yticks(rotation=0, ha='right')
     ax.yaxis.set_ticklabels(labels)
 
+    plt.gcf().subplots_adjust(left=0.2, bottom=0.27)
+
+    if(len(file_path) > 0):
+        plt.savefig(file_path.format("confusion_matrix.png"))
+        pd.DataFrame(
+            data={"y_true": y_true, "y_pred": y_pred}).to_csv(
+                file_path.format("test_set_predictions.csv"))
+
+    plt.show()
+
 # ---- USED MODELS ----
 
-def do_logistic_regression(X_train, y_train, X_test, y_test):
+def do_logistic_regression(X_train, y_train, X_test, y_test, file_path=""):
     #scaling for logistic regression
     scaler = StandardScaler().fit(X_train)
 
     model_log_res = LogisticRegression(random_state=0)
     model_log_res.fit(scaler.transform(X_train), y_train)
     predictions_log_res = model_log_res.predict(scaler.transform(X_test))
-    do_classification_report(y_test, predictions_log_res, "Logistic Regression")
+    do_classification_report(y_test, predictions_log_res, "Logistic Regression", file_path)
 
-def do_lda(X_train, y_train, X_test, y_test):
+def do_lda(X_train, y_train, X_test, y_test, file_path=""):
     model_lda = LinearDiscriminantAnalysis()
     model_lda.fit(X_train, y_train)
     predictions_lda = model_lda.predict(X_test)
-    do_classification_report(y_test, predictions_lda, "LDA")
+    do_classification_report(y_test, predictions_lda, "LDA", file_path)
 
-def do_svm(X_train, y_train, X_test, y_test):
+def do_svm(X_train, y_train, X_test, y_test, file_path=""):
     #scaling for SVM
     scaler = StandardScaler().fit(X_train)
 
     model_svm = LinearSVC()
     model_svm.fit(scaler.transform(X_train), y_train)
     predictions_svm = model_svm.predict(scaler.transform(X_test))
-    do_classification_report(y_test, predictions_svm, "SVM")
+    do_classification_report(y_test, predictions_svm, "SVM", file_path)
 
-def do_mlp(X_train, y_train, X_test, y_test):
+def do_mlp(X_train, y_train, X_test, y_test, file_path=""):
     model_mlp = MLPClassifier()
     model_mlp.fit(X_train, y_train)
     predictions_mlp = model_mlp.predict(X_test)
-    do_classification_report(y_test, predictions_mlp, "MLP")
+    do_classification_report(y_test, predictions_mlp, "MLP", file_path)
