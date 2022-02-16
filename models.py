10de76856e537395b8f1efa192c56465a18227d3
PaulErpenstein <paul.erpenstein@gmail.com>
2022-02-16 15:06:09 +0100

    Safety commit

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ from sklearn.neural_network import MLPClassifier
 
 import joblib
 
-def do_classification_report(y_true, y_pred, name, file_path=""):
+def do_classification_report(y_true, y_pred, name, labels, file_path=""):
     print("Classification report for {}".format(name))
     print("Accuracy score: {}".format(metrics.accuracy_score(y_true, y_pred)))
     print("Recall score: {}".format(metrics.recall_score(y_true, y_pred, average="weighted")))
@@ -27,7 +27,6 @@ def do_classification_report(y_true, y_pred, name, file_path=""):
     ax.set_xlabel('Predicted labels')
     ax.set_ylabel('True labels') 
     ax.set_title('Confusion Matrix')
-    labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
     ax.xaxis.set_ticklabels(labels)
     plt.xticks(rotation=90, ha='right')
     plt.yticks(rotation=0, ha='right')
@@ -49,40 +48,40 @@ def export_model(model, path):
 
 # ---- USED MODELS ----
 
-def do_logistic_regression(X_train, y_train, X_test, y_test, file_path=""):
+def do_logistic_regression(X_train, y_train, X_test, y_test, labels, file_path=""):
     #scaling for logistic regression
     scaler = StandardScaler().fit(X_train)
 
     model_log_res = LogisticRegression(random_state=0)
     model_log_res.fit(scaler.transform(X_train), y_train)
     predictions_log_res = model_log_res.predict(scaler.transform(X_test))
-    do_classification_report(y_test, predictions_log_res, "Logistic Regression", file_path)
+    do_classification_report(y_test, predictions_log_res, "Logistic Regression", labels, file_path)
 
     export_model(model_log_res, file_path)
 
-def do_lda(X_train, y_train, X_test, y_test, file_path=""):
+def do_lda(X_train, y_train, X_test, y_test, labels, file_path=""):
     model_lda = LinearDiscriminantAnalysis()
     model_lda.fit(X_train, y_train)
     predictions_lda = model_lda.predict(X_test)
-    do_classification_report(y_test, predictions_lda, "LDA", file_path)
+    do_classification_report(y_test, predictions_lda, "LDA", labels, file_path)
 
     export_model(model_lda, file_path)
 
-def do_svm(X_train, y_train, X_test, y_test, file_path=""):
+def do_svm(X_train, y_train, X_test, y_test, labels, file_path=""):
     #scaling for SVM
     scaler = StandardScaler().fit(X_train)
 
     model_svm = LinearSVC()
     model_svm.fit(scaler.transform(X_train), y_train)
     predictions_svm = model_svm.predict(scaler.transform(X_test))
-    do_classification_report(y_test, predictions_svm, "SVM", file_path)
+    do_classification_report(y_test, predictions_svm, "SVM", labels, file_path)
 
     export_model(model_svm, file_path)
 
-def do_mlp(X_train, y_train, X_test, y_test, file_path=""):
+def do_mlp(X_train, y_train, X_test, y_test, labels, file_path=""):
     model_mlp = MLPClassifier()
     model_mlp.fit(X_train, y_train)
     predictions_mlp = model_mlp.predict(X_test)
-    do_classification_report(y_test, predictions_mlp, "MLP", file_path)
+    do_classification_report(y_test, predictions_mlp, "MLP", labels, file_path)
 
     export_model(model_mlp, file_path)
